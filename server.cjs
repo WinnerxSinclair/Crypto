3216ad2a20d18f94157e81f61fde4eaa06feaefb
WinnerxSinclair <cobrasuperkek@gmail.com>
2024-12-27 00:51:44 -0600

    deploy test

--- server.cjs
+++ server.cjs
@@ -5,7 +5,7 @@ const cors = require('cors');
 console.log('Starting the server...');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: ['http://localhost:5173', 'https://crypto-production-5a21.up.railway.app']
