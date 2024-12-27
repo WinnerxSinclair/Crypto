36f5707018b5241c59a49e070c1b6c1f61156c9c
WinnerxSinclair <cobrasuperkek@gmail.com>
2024-12-27 00:24:23 -0600

    deploy

--- server.cjs
+++ server.cjs
@@ -9,7 +9,10 @@ const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html')); 
+});
 //chart
 app.get('/crypto/:id/:timeframe', async (req, res) => {
   const { id, timeframe } = req.params;
